7a4940bfb38f54fde906cde4d37e1d9711e552cc
anhkhoa2109.it@gmail.com <anhkhoa2109.it@gmail.com>
2025-03-21 08:06:20 +0700

    commit retrofit

--- AppKiemTraAndroidT6/app/src/main/java/com/android/projectnhom/retrofit/RetrofitClient.java
+++ AppKiemTraAndroidT6/app/src/main/java/com/android/projectnhom/retrofit/RetrofitClient.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/*Nguyễn Hoàng Anh Khoa - 22110352*/
 public class RetrofitClient {
 
 
