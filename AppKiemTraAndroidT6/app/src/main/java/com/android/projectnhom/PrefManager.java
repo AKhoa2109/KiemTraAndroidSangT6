402a25fec404b25c442ee8644de07099467f1f6a
anhkhoa2109.it@gmail.com <anhkhoa2109.it@gmail.com>
2025-03-21 08:43:12 +0700

    commit để pull code

---
+++ AppKiemTraAndroidT6/app/src/main/java/com/android/projectnhom/PrefManager.java
@@ -0,0 +1,32 @@
+package com.android.projectnhom;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/*Nguyễn Hoàng Anh Khoa - 22110352*/
+public class PrefManager {
+    Context context;
+    PrefManager(Context context){
+        this.context = context;
+    }
+
+    public void saveLoginDetails(String email, String password){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Email",email);
+        editor.putString("Password",password);
+        editor.commit();
+    }
+
+    public String getEmail(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("Email","");
+    }
+
+    public boolean isUserLogOut(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("LoginDetails",Context.MODE_PRIVATE);
+        boolean isEmailEmpty = sharedPreferences.getString("Email","").isEmpty();
+        boolean isPasswordEmpty = sharedPreferences.getString("Password","").isEmpty();
+        return isEmailEmpty || isPasswordEmpty;
+    }
+}
