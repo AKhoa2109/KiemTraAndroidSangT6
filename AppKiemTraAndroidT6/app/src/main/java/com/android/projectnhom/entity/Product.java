eded4e4fb28b66b0edee3522dad06e419514483c
LyHung10 <lyhung10nctlop95@gmail.com>
2025-03-21 10:05:31 +0700

    them Product

---
+++ AppKiemTraAndroidT6/app/src/main/java/com/android/projectnhom/entity/Product.java
@@ -0,0 +1,26 @@
+package com.android.projectnhom.entity;
+
+//Nguyễn Tuấn Thành - 22110418
+import com.google.gson.annotations.SerializedName;
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, int id, double price, String images, String createdAt) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.images = images;
+        this.createdAt = createdAt;
+    }
+
+    private String images;
+    private String createdAt;
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public String getImages() { return images; }
+    public String getCreatedAt() { return createdAt; }
+}
