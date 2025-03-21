a22624bf086306a039dbdda702a80c889aa0cb21
LyHung10 <lyhung10nctlop95@gmail.com>
2025-03-21 08:43:52 +0700

    merge dev_HUng

---
+++ APIKiemTraAndroidT6/src/main/java/vn/ute/KiemTraAndroid/service/CategoryService.java
@@ -0,0 +1,22 @@
+// Nguyễn Lý Hùng - 22110337
+package vn.ute.KiemTraAndroid.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import vn.ute.KiemTraAndroid.entity.Category;
+import vn.ute.KiemTraAndroid.repository.CategoryRepository;
+@Service
+
+public class CategoryService {
+	private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}
